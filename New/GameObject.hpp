08ff798cce0542f165e0e3c0d9bf713b4c43a37a
tn26205 <23020690@vnu.edu.vn>
2024-04-08 20:14:58 +0700

    update GameObject.hpp

--- New/GameObject.hpp
+++ New/GameObject.hpp
@@ -5,15 +5,25 @@
 
 class GameObject{
 public:
+
+int SCREEN_WIDTH = 1536;
+int SCREEN_HEIGHT = 768;
+    int x_pos;
+   int y_pos;
+
     GameObject(const char* texturesheet, /*SDL_Renderer* ren,*/ int x, int y,int w, int h);
     ~GameObject();
 
+    //void HandleInputAction(SDL_Event events);
+    void HandleMove();
    void Update();
    void Render();
+   int getX() const { return x_pos;}
+   int getY() const { return y_pos;}
+   int getWidth() const { return destRect.w;}
+   int getHeight() const { return destRect.h;}
 
 private:
-    int xpos;
-    int ypos;
 
     SDL_Texture* objTexture;
     SDL_Rect srcRect, destRect;
