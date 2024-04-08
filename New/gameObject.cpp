130cebae584e957ce162b81e0ddd696054a46525
tn26205 <23020690@vnu.edu.vn>
2024-04-08 20:14:03 +0700

    update gameObject.cpp

--- New/gameObject.cpp
+++ New/gameObject.cpp
@@ -1,15 +1,30 @@
 #include "GameObject.hpp"
 #include "TextureManager.hpp"
+#include "game.hpp"
 
 GameObject::GameObject(const char* texturesheet,/* SDL_Renderer* ren,*/ int x, int y, int w, int h)
 {
     //renderer = ren;
     objTexture = TextureManager::LoadTexture(texturesheet/*, ren*/);
-    xpos = x;
-    ypos = y;
+    x_pos = x;
+    y_pos = y;
 
     srcRect.w = w;
     srcRect.h = h;
+}
+
+void GameObject::HandleMove()
+{
+    destRect.x += x_pos;
+    if(destRect.x < 0 || destRect.x + srcRect.w >= SCREEN_WIDTH)
+    {
+        destRect.x -= x_pos;
+    }
+    destRect.y += y_pos;
+    if(destRect.y < 0 || destRect.y + srcRect.h >= SCREEN_HEIGHT)
+    {
+        destRect.x -= x_pos;
+    }
 }
 
 void GameObject::Update()
@@ -18,10 +33,11 @@ void GameObject::Update()
     srcRect.x = 0;
     srcRect.y = 0;
 
-    destRect.x = xpos;
-    destRect.y = ypos;
+    destRect.x = x_pos;
+    destRect.y = y_pos;
     destRect.w = srcRect.w;
     destRect.h = srcRect.h;
+
 }
 
 void GameObject::Render()
