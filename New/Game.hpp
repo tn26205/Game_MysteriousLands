3a05e71ec0200f8581caa8943d859c7637d84e06
tn26205 <23020690@vnu.edu.vn>
2024-04-14 10:03:03 +0700

    Game.hpp

--- New/Game.hpp
+++ New/Game.hpp
@@ -2,10 +2,16 @@
 
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_image.h"
+#include "TextureManager.hpp"
+#include "GameObject.hpp"
+#include "Map.hpp"
+#include "GameNonogram.hpp"
+
 #include <iostream>
 #include <vector>
 
 class TextureManager;
+
 class Game
 {
 public:
@@ -19,19 +25,31 @@ public:
 
     void logErrorAndExit(const char* msg, const char* error);
     void initSDL(const char* WINDOW_TITLE, int x_pos, int y_pos, int SCREEN_WIDTH, int SCREEN_HEIGHT,bool fullScreen);
+    void handleMouse();
+    void handleKeyDownEvent(SDL_Keycode keycode);
+    void handleMousClickEvent(SDL_MouseButtonEvent& buttonEvent);
+    void handleNonogram(int mouse_x, int mouse_y);
     void handleEvents();
 	void update();
 	void render();
 	void clean();
 	void Start();
+	void ButtonNonogram();
 
 	bool running() { return isRunning; }
+	//bool Nonogram() {return isNonogram;}
     static SDL_Renderer* renderer;
+
+    SDL_Window* window;
+
+    bool isRunning = true;
+    bool isNonogram = true;
+
 private:
 
-    bool isRunning = false;
     int srcRect_w,srcRect_h;
-    SDL_Window* window;
+    int mouse_x, mouse_y;
+
 
     SDL_Event event;
 
