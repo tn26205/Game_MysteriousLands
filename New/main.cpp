59610bb8c8ed804d1afb0946478158943bff5518
tn26205 <23020690@vnu.edu.vn>
2024-04-14 09:59:04 +0700

    game.cpp

--- New/main.cpp
+++ New/main.cpp
@@ -1,48 +1,41 @@
 #include "Game.hpp"
+SDL_Window* window;
+const int SCREEN_WIDTH = 1536;
+const int SCREEN_HEIGHT = 768;
 
-int SCREEN_WIDTH = 1536;
-int SCREEN_HEIGHT = 768;
+const char*WINDOW_TITLE = "MY GAME";
 
-const char* WINDOW_TITLE = "My Game";
-
-Game *game = nullptr;
+Game* game = nullptr;
 
 int main(int argc, char* argv[])
 {
-    const int FPS=50; //(Frame Per Second: so luong khung hinh muon chay trong mot giay)
-    const int frameDelay = 1000/FPS;
-    Uint32 frameStart;
-
-    int frameTime;
-
     game = new Game();
 
     game->initSDL(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, false);
 
-    /*background = new GameObject("Game Graphics/newbackground.png",0,0,1500,750);
+    game->Start();
+    const int FPS=50; //(Frame Per Second: so luong khung hinh muon chay trong mot giay)
+    const int frameDelay = 1000/FPS;
 
-    background->Update();
-    SDL_RenderClear(Game::renderer);
-    background->Render();
-    SDL_RenderPresent(Game::renderer);*/
-    //game->Start();
+    Uint32 frameStart;
+    int frameTime;
     while(game->running()){
 
         frameStart = SDL_GetTicks();
 
-        game->handleEvents();
-        game->update();
-        game->render();
+       game->update();
+       game->render();
 
-        frameTime = SDL_GetTicks() - frameStart;
+       game->handleEvents();
 
-        if(frameDelay > frameTime)
+       frameTime = SDL_GetTicks() - frameStart;
+       if(frameDelay > frameTime)
         {
             SDL_Delay(frameDelay-frameTime);
         }
     }
 
     game->clean();
-
+    delete game;
     return 0;
 }
