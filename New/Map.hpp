f731c80f2c3bed9571de8db20ef2856cc7684e32
tn26205 <23020690@vnu.edu.vn>
2024-04-14 10:06:25 +0700

    Map.hpp

--- New/Map.hpp
+++ New/Map.hpp
@@ -1,6 +1,10 @@
 #pragma once
+#include <fstream>
 
 #include "Game.hpp"
+#include "TextureManager.hpp"
+#include "GameObject.hpp"
+#include "GameNonogram.hpp"
 
 class Map{
 public:
@@ -9,13 +13,22 @@ public:
 
     void LoadMap(const char* filepath);
     void DrawMap();
+    void mapNgram();
+    void LoadNgram(const char* filepath);
+    void DrawNgram(int digit,int pos_x, int pos_y);
+
+    int mapn[15][15];
 
 private:
     SDL_Rect src, dest;
+    SDL_Rect src_n, dest_n;
 
     SDL_Texture* grass;
+    SDL_Texture* black;
+    SDL_Texture* red;
+    SDL_Texture* x_black;
+    SDL_Texture* x_red;
 
     int map[24][48];
-
 };
 
