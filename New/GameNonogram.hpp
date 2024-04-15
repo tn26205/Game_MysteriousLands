7030f20385e367ef872f4dd5111f24de3f8d8b3c
tn26205 <23020690@vnu.edu.vn>
2024-04-15 23:27:22 +0700

    void Suggest

--- New/GameNonogram.hpp
+++ New/GameNonogram.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
 #include <SDL2/SDL.h>
+#include <vector>
 
-//#include "GameObject.hpp"
 #include "Map.hpp"
-//#include "Game.hpp"
 
 const int START_X_GRID = 600;
 const int START_Y_GRID = 200;
@@ -19,9 +18,14 @@ public:
     void LoadNgram(const char* filepath);
     void handleEventNgramClickLeft(int &mouse_x, int &mouse_y);
     void handleEventNgramClickRight(int &mouse_x, int &mouse_y);
+    void Suggest(int a[15][15]);
 
-    //int mapn[15][15];
+    vector<vector<int>> sg_row;
+    vector<vector<int>> sg_col;
+
+    vector<int> row_segments;
+    vector<int> col_segments;
+
 private:
     SDL_Renderer *renderer;
-    int cnt=0;
 };
--- New/gameNonogram.cpp
+++ New/gameNonogram.cpp
@@ -15,22 +15,8 @@ GameNgram::GameNgram()
 
 GameNgram::~GameNgram()
 {}
-/*void GameNgram::LoadNgram(const char* filepath)
-{
-    std::ifstream file(filepath);
-    if(!file.is_open()) std::cout << "error";
-
-    for(int i=0;i<24;i++){
-        for(int j=0;j<48;j++){
-            file >> mapn[i][j];
-        }
-    }
-    file.close();
-
-}*/
 void GameNgram::handleEventNgramClickLeft(int &mouse_x, int &mouse_y)
 {
-    //LoadNgram("mapNgram.txt");
     std::cout << "Click left: " << mouse_x << " " << mouse_y << std::endl;
     int pos_x = (int)((mouse_y - START_Y_GRID) / PUZZLE_SIZE);
     int pos_y = (int)((mouse_x - START_X_GRID) / PUZZLE_SIZE);
@@ -68,3 +54,68 @@ void GameNgram::handleEventNgramClickRight(int &mouse_x, int &mouse_y)
         std::cout << std::endl;
     }
 }
+
+void GameNgram::Suggest(int a[15][15]){
+    sg_row.clear();
+    sg_col.clear();
+
+    for (int i = 0; i < 15; i++) {
+        row_segments.clear();
+        int check = 0;
+
+        for (int j = 0; j < 15; j++) {
+            if (a[i][j] == 0) {
+                if (check > 0) {
+                    row_segments.push_back(check);
+                    check = 0;
+                }
+            } else if (a[i][j] == 1) {
+                check++;
+            }
+        }
+
+        if (check > 0) {
+            row_segments.push_back(check);
+        }
+
+        sg_row.push_back(row_segments);
+    }
+
+    for (int i = 0; i < 15; i++) {
+        for (int j = 0; j < sg_row[i].size(); j++) {
+            cout << sg_row[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+
+    for (int j = 0; j < 15; j++) {
+        row_segments.clear();
+        int check = 0;
+
+        for (int i = 0; i < 15; i++) {
+            if (a[i][j] == 0) {
+                if (check > 0) {
+                    row_segments.push_back(check);
+                    check = 0;
+                }
+            } else if (a[i][j] == 1) {
+                check++;
+            }
+        }
+
+        if (check > 0) {
+            row_segments.push_back(check);
+        }
+
+        sg_col.push_back(row_segments);
+    }
+
+    for (int i = 0; i < 15; i++) {
+        for (int j = 0; j < sg_col[i].size(); j++) {
+            cout << sg_col[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
