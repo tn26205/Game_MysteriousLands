56145cbcdfdc3f8c0b8913de0ebe9eea4f764d1f
tn26205 <23020690@vnu.edu.vn>
2024-04-24 22:41:36 +0700

    checkCollision

--- New/GameObject.hpp
+++ New/GameObject.hpp
@@ -8,17 +8,19 @@ public:
 int SCREEN_WIDTH = 1536;
 int SCREEN_HEIGHT = 768;
     int x_pos;
-   int y_pos;
+    int y_pos;
 
     GameObject(const char* texturesheet,int x, int y,int w, int h);
     ~GameObject();
-    void free(SDL_Texture *texture);
-   void Update();
-   void Render();
-   int getX() const { return x_pos;}
-   int getY() const { return y_pos;}
-   int getWidth() const { return destRect.w;}
-   int getHeight() const { return destRect.h;}
+
+    void Update();
+    void Render();
+    int getX() const { return x_pos;}
+    int getY() const { return y_pos;}
+    int getWidth() const { return destRect.w;}
+    int getHeight() const { return destRect.h;}
+
+    bool checkCollision(const GameObject* obj_1, const GameObject* obj_2);
 
     SDL_Rect srcRect, destRect;
 
--- New/gameObject.cpp
+++ New/gameObject.cpp
@@ -2,36 +2,47 @@
 #include "TextureManager.hpp"
 #include "game.hpp"
 
-GameObject::GameObject(const char* texturesheet,/* SDL_Renderer* ren,*/ int x, int y, int w, int h)
+GameObject::GameObject(const char* texturesheet, int x, int y, int w, int h)
 {
-    //renderer = ren;
-    objTexture = TextureManager::LoadTexture(texturesheet/*, ren*/);
+    objTexture = TextureManager::LoadTexture(texturesheet);
     x_pos = x;
     y_pos = y;
 
     srcRect.w = w;
     srcRect.h = h;
+
+    srcRect.x = 0;
+    srcRect.y = 0;
+}
+
+GameObject::~GameObject()
+{
+    if (objTexture != nullptr)
+    {
+        SDL_DestroyTexture(objTexture);
+        objTexture = nullptr;
+    }
 }
 
 void GameObject::Update()
 {
-
-    srcRect.x = 0;
-    srcRect.y = 0;
-
     destRect.x = x_pos;
     destRect.y = y_pos;
     destRect.w = srcRect.w;
     destRect.h = srcRect.h;
 
 }
-void GameObject::free(SDL_Texture *texture)
-{
-    SDL_DestroyTexture(texture);
-    texture=nullptr;
-}
 void GameObject::Render()
 {
     SDL_RenderCopy(Game::renderer, objTexture, &srcRect, &destRect);
 }
+
+bool GameObject::checkCollision(const GameObject* obj_1, const GameObject* obj_2)
+{
+    return (obj_1->destRect.x < obj_2->destRect.x + obj_2->destRect.w/2-20 &&
+            obj_1->destRect.x + obj_1->destRect.w > obj_2->destRect.x &&
+            obj_1->destRect.y < obj_2->destRect.y + obj_2->destRect.h &&
+            obj_1->destRect.y + obj_1->destRect.h > obj_2->destRect.y);
+}
+
 
